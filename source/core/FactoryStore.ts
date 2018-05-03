602b6eb1369ed711b8a9c96c425e351707b282e5
Jakob Wischner <jakob.wischner@gmail.com>
2018-05-03 18:39:49 +0200

    some implementation

--- source/core/FactoryStore.ts
+++ source/core/FactoryStore.ts
@@ -1,5 +1,27 @@
 import MapObject from './MapObject';
 
+// TODO: Implement Iterator
 export default class FactoryStore {
-    
+    private mapObjects: MapObject[];
+
+    /**
+     * getObjectByID
+     */
+    public getObjectByID(ID: number) {}
+
+    /**
+     * createNPC
+     */
+    public createNPC() {}
+
+    /**
+     * createUnit
+     */
+    public createUnit() {}
+
+    /**
+     * removeObject
+     */
+    public removeObject(ID: number) {}
+
 }
